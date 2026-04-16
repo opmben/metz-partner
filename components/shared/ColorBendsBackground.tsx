8d999cefa078e251d1561839e4762197c4a4c766
opmben <benedikt231203@gmail.com>
2026-04-17 01:37:52 +0200

    Modify ColorBendsBackground with new settings
    Updated ColorBends component properties and added a new instance with different colors.

--- components/shared/ColorBendsBackground.tsx
+++ components/shared/ColorBendsBackground.tsx
@@ -13,15 +13,39 @@ export function ColorBendsBackground() {
     >
       <ColorBends
         colors={['#080808', '#0d1a00', '#1a2e00', '#C8FF00', '#080808', '#111111', '#0a1400', '#080808']}
-        speed={1.0}
+        rotation={90}
+        speed={1}
+        transparent={false}
+        autoRotate={0}
+        scale={1}
         frequency={1.2}
-        noise={0.49}
-        bandWidth={0.14}
-        rotation={92}
-        fadeTop={0.75}
-        iterations={2}
-        intensity={1.3}
+        warpStrength={1}
+        mouseInfluence={1}
+        parallax={0.5}
+        noise={0.4}
+        iterations={1}
+        intensity={1.5}
+        bandWidth={6}
       />
     </div>
   )
 }
+
+<div style={{ width: '1080px', height: '1080px', position: 'relative' }}>
+  <ColorBends
+    rotation={90}
+    speed={1}
+    colors={["#5227FF","#FF9FFC"]}
+    transparent={false}
+    autoRotate={0}
+    scale={1}
+    frequency={1.2}
+    warpStrength={1}
+    mouseInfluence={1}
+    parallax={0.5}
+    noise={0.4}
+    iterations={1}
+    intensity={1.5}
+    bandWidth={6}
+  />
+</div>
