8f717ad428e357ac64113702aa690f75d558663a
opmben <benedikt231203@gmail.com>
2026-04-17 01:55:40 +0200

    Adjust ColorBendsBackground properties for optimization

--- components/shared/ColorBendsBackground.tsx
+++ components/shared/ColorBendsBackground.tsx
@@ -14,38 +14,19 @@ export function ColorBendsBackground() {
       <ColorBends
         colors={['#080808', '#0d1a00', '#1a2e00', '#C8FF00', '#080808', '#111111', '#0a1400', '#080808']}
         rotation={90}
-        speed={0.4}
+        speed={0.35}
         transparent={false}
         autoRotate={0}
         scale={1}
-        frequency={1.2}
+        frequency={1.4}
         warpStrength={1}
-        mouseInfluence={1}
-        parallax={0.5}
-        noise={0.3}
+        mouseInfluence={0.6}
+        parallax={1}
+        noise={0.25}
         iterations={2}
-        intensity={1.5}
+        intensity={1}
         bandWidth={6}
       />
     </div>
   )
 }
-
-<div style={{ width: '1080px', height: '1080px', position: 'relative' }}>
-  <ColorBends
-    rotation={90}
-    speed={1}
-    colors={["#5227FF","#FF9FFC"]}
-    transparent={false}
-    autoRotate={0}
-    scale={1}
-    frequency={1.2}
-    warpStrength={1}
-    mouseInfluence={1}
-    parallax={0.5}
-    noise={0.4}
-    iterations={1}
-    intensity={1.5}
-    bandWidth={6}
-  />
-</div>
